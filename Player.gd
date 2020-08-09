c5790df7b9c5c87942586026b0b8796f0e0deaee
tomual <tomual@users.noreply.github.com>
2020-08-09 23:53:56 +0800

    Main scene setup

---
+++ Player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+const DIR_B = "b"
+const DIR_T = "t"
+const DIR_L = "l"
+const DIR_R = "r"
+
+var speed = 200
+var hp = 10
+var screen_size
+var facing
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	$AnimatedSprite.play()
+
+
+func _process(delta):
+	if (can_move()):
+		var velocity = Vector2()
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+			facing = DIR_R
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+			facing = DIR_L
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+			facing = DIR_B
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1
+			facing = DIR_T
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite.animation = str(facing, "_idle")
+		else:
+			$AnimatedSprite.animation = str(facing, "_idle")
+	
+		position += velocity * delta
+		position.x = clamp(position.x, 30, screen_size.x - 30)
+		position.y = clamp(position.y, 75, screen_size.y - 45)
+
+
+func can_move():
+	return hp > 0
